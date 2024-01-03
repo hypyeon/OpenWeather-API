99c5c363c937bc1e6afb39e5a8f98dc63573c88d
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-01-02 22:29:42 -0800

    API practice homework done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
 import './css/styles.scss';
+
+// Business Logic
+
+function getWeather(city) {
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+  
+    request.addEventListener("loadend", function() {
+        const response = JSON.parse(this.responseText);
+        if (this.status === 200) {
+            printElements(response, city);
+        } else {
+            printError(this, city);
+        }
+    });
+    /*
+    request.addEventListener("readystatechange", function() {
+        console.log(this.readyState);
+    });
+    */
+
+    request.open("GET", url, true);
+    request.send();
+}
+
+// UI Logic
+
+function printElements(apiResponse, city) {
+    document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.
+    The temperature in Kelvins is ${apiResponse.main.temp} degrees.`;
+}
+
+function printError(request, city) {
+    document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}`;
+}
+
+function handleFormSubmission(event) {
+    event.preventDefault();
+    const city = document.querySelector('#location').value;
+    document.querySelector('#location').value = null;
+    getWeather(city);
+}
+
+window.addEventListener("load", function() {
+    document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
